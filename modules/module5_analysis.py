9b5e72a1f3dac2094ce178f2854455e1611a77e6
burhanmsib <167619728+burhanmsib@users.noreply.github.com>
2026-05-06 13:44:05 +0700

    Update module5_analysis.py

--- modules/module5_analysis.py
+++ modules/module5_analysis.py
@@ -386,7 +386,6 @@ def analyze_segment(samples):
 
     # ===== CURRENT =====
     cur_dirs, cur_spds = [], []
-    cur_spds = [c for c in cur_spds if c > 2]  # buang noise kecil
     for s in samples:
         u = s.get("current", {}).get("u")
         v = s.get("current", {}).get("v")
@@ -398,6 +397,7 @@ def analyze_segment(samples):
             cur_dirs.append(d)
             cur_spds.append(spd)
 
+    cur_spds = [c for c in cur_spds if c > 2]
     cur_txt = "Variable"
     if cur_dirs and cur_spds:
         d_start = cur_dirs[0]
