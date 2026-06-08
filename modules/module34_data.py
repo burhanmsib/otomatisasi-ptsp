53a5e19672c1b3d3c3dbc8f985fe9d68513bbfc0
burhanmsib <167619728+burhanmsib@users.noreply.github.com>
2026-06-08 17:24:39 +0700

    Update module34_data.py

--- modules/module34_data.py
+++ modules/module34_data.py
@@ -683,8 +683,6 @@ def extract_hourly_weather(ds_wave, ds_cur, ds_rain, t, lat, lon):
 # =========================
 def process_module34(row, polyline, tz="WIB", ds_wave=None, ds_cur=None, ds_rain=None):
 
-    # dt_local = normalize_date(row["Tanggal Koordinat"])
-
     dt_local = normalize_date(
         row["Tanggal Koordinat"]
     )
