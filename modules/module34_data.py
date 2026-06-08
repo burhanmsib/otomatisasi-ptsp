541691c348fa4f664663ca70796250ca93c33e23
burhanmsib <167619728+burhanmsib@users.noreply.github.com>
2026-06-08 17:08:18 +0700

    Update module34_data.py

--- modules/module34_data.py
+++ modules/module34_data.py
@@ -689,9 +689,9 @@ def process_module34(row, polyline, tz="WIB", ds_wave=None, ds_cur=None, ds_rain
         row["Tanggal Koordinat"]
     )
     
-    raise Exception(
-        f"DEBUG TANGGAL: {row['Tanggal Koordinat']} -> {dt_local}"
-    )
+    # raise Exception(
+    #     f"DEBUG TANGGAL: {row['Tanggal Koordinat']} -> {dt_local}"
+    # )
     
     if dt_local is None:
         return None
